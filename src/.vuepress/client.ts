521fd194200e7392475bbfb86e8165552cdfe9ec
懶魚 <189705063+94lanyu@users.noreply.github.com>
2025-05-11 23:39:45 +0800

    【Hope主題】v50 GA (Google Analytics) 換成 GTM (Google Tag Manager)

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,20 +1,20 @@
 import { defineClientConfig } from '@vuepress/client';
+import { onMounted, h } from 'vue';
 
 export default defineClientConfig({
-    onMounted() {
-        // 建立 iframe 元素
-        const iframe = document.createElement('iframe');
-        iframe.src = 'https://www.googletagmanager.com/ns.html?id=GTM-MTHMCMQT';
-        iframe.height = '0';
-        iframe.width = '0';
-        iframe.style.display = 'none';
-        iframe.style.visibility = 'hidden';
+    setup() {
+        onMounted(() => {
+            const iframe = document.createElement('iframe');
+            iframe.src = 'https://www.googletagmanager.com/ns.html?id=GTM-MTHMCMQT';
+            iframe.height = '0';
+            iframe.width = '0';
+            iframe.style.display = 'none';
+            iframe.style.visibility = 'hidden';
 
-        // 建立 noscript 並包住 iframe
-        const noscript = document.createElement('noscript');
-        noscript.appendChild(iframe);
+            const noscript = document.createElement('noscript');
+            noscript.appendChild(iframe);
 
-        // 插入到 <body> 一開始的位置
-        document.body.insertBefore(noscript, document.body.firstChild);
+            document.body.insertBefore(noscript, document.body.firstChild);
+        });
     },
 });
