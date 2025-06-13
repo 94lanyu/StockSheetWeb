30143cc4ae5809d6765216bc8da638c5e370414a
懶魚 <189705063+94lanyu@users.noreply.github.com>
2025-06-14 06:33:22 +0800

    【Hope主題】v60 GTM 埋 code 改在 config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,5 @@
-import { viteBundler } from "@vuepress/bundler-vite";
-import { defineUserConfig } from "vuepress";
+import {viteBundler} from "@vuepress/bundler-vite";
+import {defineUserConfig} from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
@@ -9,9 +9,17 @@ export default defineUserConfig({
     title: "全自動股票管理表",
     description: "自動化股票管理excel(Google試算表)，免下載，複製立即可用。免費試用、簡單股票記帳、股票管理表、自動更新股價、股票儀表版",
 
-    // 網站圖標 favicon.ico
     head: [
-        ['link', { rel: 'icon', href: '/logo.ico' }]
+        // 網站圖標 favicon.ico
+        ['link', {rel: 'icon', href: '/logo.ico'}],
+        // Google Console search html 擁有全驗證
+        // ['meta', {name: 'google-site-verification', content: 'AdvLMReW9RFoWDOXEAm_koqL82b1ZwwKxlgrhaQtRnQ'}],
+        // Google Tag Manager 埋 code
+        ['script', {}, `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MTHMCMQT');`]
     ],
 
     theme,
