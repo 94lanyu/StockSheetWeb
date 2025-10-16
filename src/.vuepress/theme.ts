a2b5aa3959698eeda89154282b03c12b17459e22
懶魚 <189705063+94lanyu@users.noreply.github.com>
2025-10-16 22:02:06 +0800

    【Hope主題】v63 修正: 啟用 SEO 外掛以生成正確的 BreadcrumbList 結構化資料

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -89,24 +89,8 @@ export default hopeTheme({
             ]
         },
 
-        // 啟用SEO，GTM 已加在 config.ts
-        // seo: {
-        //     customHead: head => {
-        //         head.push(
-        //             [
-        //                 "script",
-        //                 {},
-        //                 `
-        //                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        //                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        //                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        //                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        //                 })(window,document,'script','dataLayer','GTM-MTHMCMQT');
-        //                 `,
-        //             ]
-        //         );
-        //     }
-        // },
+        // 啟用SEO插件 (GTM 已在 config.ts 配置)
+        seo: true,
 
         // 啟用sitemap
         sitemap: {
